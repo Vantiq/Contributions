decae70af8e1f5c3d093014bed194317bbb29714
Jonathan Victorino <jonathanvictorino@Jonathans-MacBook-Pro-2.local>
2020-07-28 13:38:56 -0700

    Cleaned up comments

--- PublicOnboardingDemo/clients/UserRegistration/controllers/page/Start/widgets/RegisterButton/onClick.js
+++ PublicOnboardingDemo/clients/UserRegistration/controllers/page/Start/widgets/RegisterButton/onClick.js
@@ -1,5 +1,6 @@
     client.getWidget("RegisterButton").isDisabled = true;
     
+    // copy over form information to local variable
     var userDef = {
         email: client.data.email,
         first_name: client.data.first_name,
@@ -21,7 +22,6 @@
     function(errors)
     {
         console.log(errors);
-        //alert(JSON.stringify(errors));
         alert("You have entered an invalid email. Please try a different one.");
         client.data.email = "";
     });
