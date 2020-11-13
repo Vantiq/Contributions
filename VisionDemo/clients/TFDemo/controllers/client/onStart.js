a6f80293125a62a6c7d4aee3bb69fce290dfe3ff
Jonathan Victorino <jonathanvictorino@Jonathans-MacBook-Pro-2.local>
2020-11-13 13:59:27 -0800

    Fixed indentation and line width issues

--- VisionDemo/clients/TFDemo/controllers/client/onStart.js
+++ VisionDemo/clients/TFDemo/controllers/client/onStart.js
@@ -15,6 +15,10 @@
         http.setVantiqUrlForSystemResource("procedures");
         http.setVantiqHeaders();
         var img = new Image();
+        
+        //var results = TensorFlowOperation.processDocument(args);
+        //console.log(results);
+        
         http.execute(args,"tfExample",function(response) {
             client.getWidget("StaticHtmlObjView").html = "<pre>" + JSON.stringify(response, null ,2) + "</pre>";
 			
@@ -29,18 +33,23 @@
                     ctx.lineWidth = "2";	
                     ctx.strokeStyle = client.getWidget("BoxColor").platformWidget.rawValue;	
                     
-                    ctx.rect(result.location.left * widthScale, result.location.top * heightScale , (result.location.right - result.location.left) * widthScale, (result.location.bottom -  result.location.top) * heightScale); 
+                    ctx.rect(result.location.left * widthScale, result.location.top * heightScale , 
+                             (result.location.right - result.location.left) * widthScale, 
+                             (result.location.bottom -  result.location.top) * heightScale); 
                     
                     ctx.stroke();
                     ctx.font = '20pt Calibri';
                     ctx.fillStyle = client.getWidget("TextColor").platformWidget.rawValue;
-                    ctx.fillText(result.label + " : " + result.confidence.toFixed(2), (result.location.left < 0) ? 0 : result.location.left * widthScale , (result.location.top < 20) ? (result.location.top > 0 ? result.location.top + (20 - result.location.top) : -result.location.top + 5) * heightScale :(result.location.top - 10) * heightScale);
+                    ctx.fillText(result.label + " : " + result.confidence.toFixed(2), (result.location.left < 0) ?
+                                 0 : result.location.left * widthScale , (result.location.top < 20) ? 
+                                 (result.location.top > 0 ? result.location.top + (20 - result.location.top) : 
+                                  -result.location.top + 5) * heightScale :(result.location.top - 10) * heightScale);
                     ctx.font = '10pt Calibri';
                     
             	});
         	};
         	img.src = design;
-			var upload = client.getWidget("UploadButton");
+            var upload = client.getWidget("UploadButton");
             var refresh = client.getWidget("RefreshButton");
             upload.isDisabled = false;
             refresh.isDisabled = false;
