d69e65843a1e99cbc987ac272216485e40814ed6
Patrick Burma <pburma@gmail.com>
2022-02-03 15:44:37 -0700

    Example of a side-by-side draggable and resortable multiselect field (#12)
    * Example of a click-n-drag side-by-side MultiSelect field

---
+++ MultiSelect1/clients/MultiSelect1/controllers/page/Start/onStart.js
@@ -0,0 +1,50 @@
+
+    //Clear the list, needed for page reloads.
+    $('#allLocations li').remove();
+    $('#setLocations li').remove();
+    
+    
+    $( function() {
+        $( "#setLocations, #allLocations" ).sortable({
+            connectWith: ".connectedSortable"
+        }).disableSelection();
+    });
+
+    /* Use parameters when loading from a datatable, remove the select on "selected" block
+    
+    var setLocations = parameters.locations;
+	
+    */
+    
+    client.select("selected", {}, function(setLocations){
+        loadLocations(setLocations);
+    });
+    
+    function loadLocations(setLocations){
+    if (setLocations) {
+        setLocations.forEach(function(item, index){
+            $('#setLocations').append('<li class="setList" data-id=' + item.id +'>' + item.name + '</li>');
+        });    
+    }
+
+    client.select("locations", {}, function(results) {
+        results.forEach(function(item, index){
+            var obj = {name:item.name, id:item.id};
+            containsObject(obj, setLocations);
+        });
+    });
+	}
+        function containsObject(obj, list) {
+        var i;
+        for (i = 0; i < list.length; i++) {
+            if (list[i].name === obj.name) {
+                return true;
+            }
+        }
+        $('#allLocations').append('<li class="allList" data-id=' + obj.id +'>' + obj.name + '</li>'); 
+    }
+
+	
+
+
+
