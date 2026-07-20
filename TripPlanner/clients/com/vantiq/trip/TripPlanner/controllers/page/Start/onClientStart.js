57695467b01fbe8a4f1465de85b397ae4b21f009
Roger Blumer <rblumer4@users.noreply.github.com>
2026-07-20 14:39:55 -0700

    use isReadonly to prevent TripPlanner input until Service is initialized (#49)
    Do this in both clients, i.e. for Conversation and Discussion variants

--- TripPlanner/clients/com/vantiq/trip/TripPlanner/controllers/page/Start/onClientStart.js
+++ TripPlanner/clients/com/vantiq/trip/TripPlanner/controllers/page/Start/onClientStart.js
@@ -1,3 +1,6 @@
+    // lock the text-entry field in Conversation widget until TripPlanner service is initialized
+    client.getWidget("Conversation1").isReadOnly = true;
+
     var trip = {id: crypto.randomUUID()};
     var args = {
         entityId: trip.id,
@@ -6,4 +9,5 @@
     client.execute(args, "com.vantiq.trip.TripPlanner.tripActivate", function(collaborationId) {
         client.setCollaborationContext({id: collaborationId});
         client.data.tripId = trip.id;
+        client.getWidget("Conversation1").isReadOnly = false;  // unlock the text-entry field
     });
